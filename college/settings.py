34d9e9c174aaba8722704f25cfa3bfed50caaffd
SAKTHIVEL <sakthi007032@gmail.com>
2025-11-12 14:21:29 +0530

    Update settings.py

--- college/settings.py
+++ college/settings.py
@@ -35,7 +35,8 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS=college-o6el.onrender.com,127.0.0.1,localhost
+
 
 
 # Application definition
