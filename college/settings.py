75582bf7f73f392bd68b0893f921a0e8fe1f88b1
SAKTHIVEL <sakthi007032@gmail.com>
2025-11-12 14:20:25 +0530

    Update settings.py

--- college/settings.py
+++ college/settings.py
@@ -35,7 +35,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
